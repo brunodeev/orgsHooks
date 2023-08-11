bd94c15e48ee188fbe973b82673ee5d95e90763a
Bruno César <bcgmeireles@gmail.com>
2023-08-10 22:26:27 -0300

    feat: adjust style of card

--- src/pages/Home/components/Producer.js
+++ src/pages/Home/components/Producer.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import { View, Text, Image } from 'react-native';
+import { View, Text, Image, StyleSheet } from 'react-native';
 
 export default function Producer({ name, image, distance, stars }){
     return (
-        <View>
-            <Image source={image} accessibilityLabel={name}/>
-            <View>
-                <View>
+        <View style={styles.container}>
+            <Image source={image} accessibilityLabel={name} style={styles.image}/>
+            <View style={styles.vertical}>
+                <View style={styles.horizontal}>
                     <Text>{name}</Text>
                     <Text>{distance}</Text>
                 </View>
@@ -15,3 +15,21 @@ export default function Producer({ name, image, distance, stars }){
         </View>
     );
 };
+
+const styles = StyleSheet.create({
+    container: {
+        flexDirection: 'row',
+        backgroundColor: '#f6f6f6',
+        marginHorizontal: 14,
+        marginVertical: 8,
+        padding: 14,
+    },
+    image: {
+        height: 55,
+        width: 55,
+    },
+    horizontal: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+    },
+});
