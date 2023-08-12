980144d8863c7f0b8f64b1b6c598f41cf8827394
Bruno César <bcgmeireles@gmail.com>
2023-08-11 23:49:50 -0300

    feat: more upgrades in the cards

--- src/components/Stars.js
+++ src/components/Stars.js
@@ -1,18 +1,41 @@
-import React from 'react';
-import { View, Image, StyleSheet } from 'react-native';
+import React, { useState } from 'react';
+import { View, Image, StyleSheet, TouchableOpacity } from 'react-native';
 
 import estrela from '../assets/estrela.png';
 import estrelaCinza from '../assets/estrelaCinza.png';
 
 
-export default function Stars({ quantity: oldQuantity, edit = false, big = true }){
+export default function Stars({ quantity: oldQuantity, edit = false, big = false }){
 
     const styles = functionStyles(big);
 
+    const RenderStars = () => {
+
+        const [quantity, setQuantity] = useState(oldQuantity);
+
+        const starsList = [];
+
+        const getImage = (index) => {
+            if(index < quantity){
+                return estrela;
+            };
+            return estrelaCinza;
+        };
+
+        for(let i = 0; i < 5; i++){
+            starsList.push(
+                <TouchableOpacity key={i} onPress={() => setQuantity(i + 1)} disabled={!edit}>
+                <Image source={getImage(i)} style={styles.star}/>
+                </TouchableOpacity>
+            );
+        };
+
+        return starsList;
+    };
+
     return (
         <View style={styles.starsLine}>
-            <Image source={estrela} style={styles.star}/>
-            <Image source={estrela} style={styles.star}/>
+            <RenderStars />
         </View>
     );
 }
